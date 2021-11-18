cda484874321ce8fa65be7601a8c56eb4ca56021
Nikhil <yadavnikhil4210@gmail.com>
2021-11-18 02:52:21 -0800

    my first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+let MenuItems=document.getElementById("MenuItems");
+
+MenuItems.style.maxHeight="0px";
+
+function menutoggle(){
+    if(MenuItems.style.maxHeight=="0px")
+    {
+        MenuItems.style.maxHeight="400px"
+    }
+    else{
+        MenuItems.style.maxHeight="0px"
+    }
+}
